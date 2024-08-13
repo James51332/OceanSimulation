dc95cc48294eda20f4c895104a4ce07bd28e0cea
James51332 <jamesddadabo@gmail.com>
2024-08-12 21:57:22 -0500

    Cleaned Up Generator and Shader Code

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -13,15 +13,15 @@ namespace Waves
 Generator::Generator(Vision::RenderDevice *device)
   : renderDevice(device)
 {
-  CreateTextures();
   LoadShaders();
+  CreateTextures();
 
   Vision::BufferDesc desc;
-  desc.DebugName = "FFT Settings";
+  desc.DebugName = "Ocean Settings";
   desc.Type = Vision::BufferType::Uniform;
   desc.Usage = Vision::BufferUsage::Dynamic;
-  desc.Size = sizeof(glm::vec4);
-  desc.Data = nullptr;
+  desc.Size = sizeof(OceanSettings);
+  desc.Data = &oceanSettings;
   uniformBuffer = renderDevice->CreateBuffer(desc);
 }
 
@@ -37,55 +37,62 @@ Generator::~Generator()
   renderDevice->DestroyComputePipeline(computePS);
 }
 
-void FFT(Vision::ID image, Vision::ID computePS, std::size_t size, Vision::RenderDevice *device)
-{
-  device->SetComputeTexture(image);
-  device->DispatchCompute(computePS, "horizontalIFFT", {1, size, 1});
-  device->ImageBarrier();
-  device->DispatchCompute(computePS, "verticalIFFT", {size, 1, 1});
-}
-
 void Generator::CalculateOcean(float timestep)
 {
   renderDevice->BeginComputePass();
 
-  // Update our uniform data
-  time += timestep;
-  glm::vec4 data(time, 40.0f, 0.0f, 0.0f);
-  renderDevice->SetBufferData(uniformBuffer, &data, sizeof(glm::vec4));
+  // Update our ocean's settings
+  oceanSettings.time += timestep;
+  renderDevice->SetBufferData(uniformBuffer, &oceanSettings, sizeof(OceanSettings));
   renderDevice->SetComputeBuffer(uniformBuffer);
 
-  renderDevice->SetComputeTexture(heightMap);
-
   // Generate the phillips spectrum based on the given time
+  renderDevice->SetComputeTexture(heightMap, 0);
   renderDevice->SetComputeTexture(gaussianImage, 1);
   renderDevice->DispatchCompute(computePS, "generateSpectrum", { textureSize, textureSize, 1 });
 
-  // Generate the normal map fft
-  renderDevice->SetComputeTexture(normalMapX, 1);
-  renderDevice->SetComputeTexture(normalMapZ, 2);
-  renderDevice->DispatchCompute(computePS, "prepareNormalMap", {textureSize, textureSize, 1});
+  // Prepare the normal map FFT
+  renderDevice->SetComputeTexture(normalMapX, 0);
+  renderDevice->SetComputeTexture(normalMapZ, 1);
+  renderDevice->SetComputeTexture(heightMap, 2);
+  renderDevice->DispatchCompute(computePS, "prepareNormalMap", { textureSize, textureSize, 1 });
 
-  // Generate the normal map fft
-  renderDevice->SetComputeTexture(displacementX, 1);
-  renderDevice->SetComputeTexture(displacementZ, 2);
-  renderDevice->DispatchCompute(computePS, "prepareDisplacementMap", {textureSize, textureSize, 1});
+  // Prepare the displacement map FFT
+  renderDevice->SetComputeTexture(displacementX, 0);
+  renderDevice->SetComputeTexture(displacementZ, 1);
+  // renderDevice->SetComputeTexture(heightMap, 2); // still bound
+  renderDevice->DispatchCompute(computePS, "prepareDisplacementMap", { textureSize, textureSize, 1 });
 
-  FFT(heightMap, computePS, textureSize, renderDevice);
-  FFT(normalMapX, computePS, textureSize, renderDevice);
-  FFT(normalMapZ, computePS, textureSize, renderDevice);
-  FFT(displacementX, computePS, textureSize, renderDevice);
-  FFT(displacementZ, computePS, textureSize, renderDevice);
+  // Ensure that none of our FFTs operate before we are ready
+  renderDevice->ImageBarrier();
+
+  // Perform all of our necessary FFTs
+  auto fft = [&](Vision::ID image) {
+    renderDevice->SetComputeTexture(image);
+    renderDevice->DispatchCompute(computePS, "horizontalIFFT", { 1, textureSize, 1 });
+    renderDevice->ImageBarrier();
+    renderDevice->DispatchCompute(computePS, "verticalIFFT", { textureSize, 1, 1 });
+  };
+
+  fft(heightMap);
+  fft(normalMapX);
+  fft(normalMapZ);
+  fft(displacementX);
+  fft(displacementZ);
+
+  // Wait until our FFTs are done before finalizing
+  renderDevice->ImageBarrier();
 
   // Combine the normal maps after fft
-  renderDevice->SetComputeTexture(normalMapX, 1);
-  renderDevice->SetComputeTexture(normalMapZ, 2);
-  renderDevice->DispatchCompute(computePS, "combineNormalMap", {textureSize, textureSize, 1});
+  renderDevice->SetComputeTexture(normalMapX, 0);
+  renderDevice->SetComputeTexture(normalMapZ, 1);
+  renderDevice->DispatchCompute(computePS, "combineNormalMap", { textureSize, textureSize, 1 });
 
   // Also the displacement maps after fft
-  renderDevice->SetComputeTexture(displacementX, 1);
-  renderDevice->SetComputeTexture(displacementZ, 2);
-  renderDevice->DispatchCompute(computePS, "combineDisplacementMap", {textureSize, textureSize, 1});
+  renderDevice->SetComputeTexture(displacementX, 0);
+  renderDevice->SetComputeTexture(displacementZ, 1);
+  renderDevice->DispatchCompute(computePS, "combineDisplacementMap", { textureSize, textureSize, 1 });
+
   renderDevice->EndComputePass();
 }
 
@@ -96,15 +103,14 @@ void Generator::LoadShaders()
 
   Vision::ComputePipelineDesc desc;
   Vision::ShaderCompiler compiler;
-  
-  std::vector<Vision::ShaderSPIRV> imageFuncs = compiler.CompileFile("resources/imageFFT.glsl");
-  desc.ComputeKernels = imageFuncs;
+  compiler.CompileFile("resources/fft.compute", desc.ComputeKernels);
+  compiler.CompileFile("resources/spectrum.compute", desc.ComputeKernels);
   computePS = renderDevice->CreateComputePipeline(desc);
 }
 
 void Generator::CreateTextures()
 {
-  // delete any textures we may have remaining
+  // Delete any textures in case we are regenerating
   if (heightMap)
   {
     renderDevice->DestroyTexture2D(heightMap);
@@ -115,11 +121,13 @@ void Generator::CreateTextures()
     renderDevice->DestroyTexture2D(gaussianImage);
   }
 
-  // create our blank textures
+  // Create our blank textures
   Vision::Texture2DDesc desc;
   desc.Width = textureSize;
   desc.Height = textureSize;
   desc.PixelType = Vision::PixelType::RGBA32Float;
+  desc.MinFilter = Vision::MinMagFilter::Linear;
+  desc.MagFilter = Vision::MinMagFilter::Linear;
   desc.WriteOnly = false;
   desc.Data = nullptr;
 
@@ -130,7 +138,7 @@ void Generator::CreateTextures()
   displacementZ = renderDevice->CreateTexture2D(desc);
   gaussianImage = renderDevice->CreateTexture2D(desc);
 
-  // create our gaussian texture
+  // Create data for our sgaussian image on CPU
   std::vector<glm::vec4> randomValues(textureSize * textureSize, glm::vec4(0.0f));
   float scale = 1.0 / glm::sqrt(2.0);
   for (int i = 0; i < textureSize * textureSize; i++)
--- src/Generator.h
+++ src/Generator.h
@@ -16,29 +16,38 @@ public:
 
   void CalculateOcean(float timestep);
 
+  struct OceanSettings
+  {
+    float time = 0.0f;
+    float planeSize = 20.0f;
+    float gravity = 9.8f;
+    float scale = 0.01;
+    glm::vec2 windVelocity = glm::vec2(3.0f, 5.0f);
+    glm::vec2 dummy;
+  };
+  OceanSettings& GetOceanSettings() { return oceanSettings; }
+
   void LoadShaders();
+
   void CreateTextures();
-  
   Vision::ID GetHeightMap() const { return heightMap; }
-  Vision::ID GetNormalMap() const { return normalMapX; } 
+  Vision::ID GetNormalMap() const { return normalMapX; }
   Vision::ID GetDisplacementMap() const { return displacementX; }
 
 private:
   Vision::RenderDevice* renderDevice = nullptr;
+  Vision::ID computePS = 0;
 
+  Vision::ID uniformBuffer = 0;
+  OceanSettings oceanSettings;
+
+  std::size_t textureSize = 1024;
   Vision::ID heightMap = 0;
   Vision::ID normalMapX = 0;
   Vision::ID normalMapZ = 0;
   Vision::ID displacementX = 0;
   Vision::ID displacementZ = 0;
   Vision::ID gaussianImage = 0;
-
-  Vision::ID uniformBuffer = 0;
-  float time = 0.0f;
-
-  Vision::ID computePS = 0;
-
-  std::size_t textureSize = 1024;
 };
 
 }
